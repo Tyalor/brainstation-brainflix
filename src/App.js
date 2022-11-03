da93a9f0900a1fd4d9a736ce2afe3fd5c300a153
Tyalor <tyalorkny@gmail.com>
2022-11-03 15:13:28 -0400

    video comments and video list

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
 import { VideoDesc } from './components/VideoDesc/VideoDesc';
 import { VideoComments } from './components/VideoComments/VideoComments';
 import { VideoList } from './components/VideoList/VideoList';
+import videosData from './data/videos.json'
+import videosDataDetails from './data/video-details.json'
 
 function App() {
+
+  // console.log(videosDataDetails)
+  // console.log(videosDataDetails[0])
+  // console.log(videosDataDetails[i].comments)
+
   return (
     <>
       <Header />
       <div>
         <VideoPlayer />
         <VideoDesc />
-        <VideoComments />
-        <VideoList />
+        <VideoComments videosDataDetails={videosDataDetails} />
+        <VideoList videosData={videosData}/>
       </div>
     </>
   );
--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,21 +1,32 @@
 import './VideoList.scss';
 import pic from '../../assets/images/Mohan-muruge.jpg'
 
-const VideoList = () => {
+const VideoList = (props) => {
+
+    let videosData = props.videosData
+
     return (
         <div className="list">
             <div className="list__container">
                 <h3 className="list__heading">Next Videos</h3>
                 <div className="list__item-container">
-                    <div className="list__item">
-                        <div className="list__img-container">
-                            <img className="list__img" src={pic} alt="" />
+                {
+
+                    videosData.map(e => {
+                        return (
+                        <div className="list__item" key={e.id}>
+                            <div className="list__img-container">
+                                <img className="list__img" src={e.image} alt="" />
+                            </div>
+                            <div className="list__copy">
+                                <h4 className="list__title">{e.title}</h4>
+                                <p className="list__user">{e.channel}</p>
+                            </div>
                         </div>
-                        <div className="list__copy">
-                            <h4 className="list__title">Become A Travel Pro In One Easy Lesson</h4>
-                            <p className="list__user">Todd Welch</p>
-                        </div>
-                    </div>
+                        )
+                    })
+                }
+                    
                 </div>
             </div>
         </div>
